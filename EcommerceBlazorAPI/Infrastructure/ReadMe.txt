e21b2c81625419d0d47e514dbb3f702f77b0ae1d
ercan kaplan <ercan.kaplan@setur.com.tr>
2024-02-04 14:39:47 +0300

    added DBContext and Product table by codefirst manner.

---
+++ EcommerceBlazorAPI/Infrastructure/ReadMe.txt
@@ -0,0 +1,9 @@
+﻿Package Manager Console
+cd EcommerceBlazorAPI
+
+dotnet ef migrations add CreateInitial
+dotnet ef database update
+
+dotnet ef migrations add SeedingData
+dotnet ef database update
+
--- EcommerceBlazorAPI/Program.cs
+++ EcommerceBlazorAPI/Program.cs
@@ -1,11 +1,22 @@
+global using Microsoft.EntityFrameworkCore;
+global using EcommerceBlazorAPI.Model;
+using EcommerceBlazorAPI.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ECommerceDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
